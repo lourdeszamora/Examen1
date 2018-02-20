a0e61dfbeec9953d4c62e4c44505efb595b847f8
LourdesZamora <lourdes.zamora@unitec.edu>
2018-02-19 21:53:32 -0600

    j

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,19 @@
 class LocationsController < ApplicationController
+    def new
+    
+    end
+    def create
+        @location = Location.new(location_params)
+
+        @location.save
+        redirect_to :action => :index
+    end
+    
+    def index
+
+    end
+     private
+    def location_params
+      params.require(:location).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
  get 'locations/new'
+ post "locations" => "locations#create"
  get 'locations/delete'
   get 'locations/edit'
    get 'locations/show'
- root to: "locations#new"
+  get 'locations/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
