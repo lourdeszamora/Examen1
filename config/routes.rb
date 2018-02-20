0249b4420b88edc70bc9f0323459e90d3299407e
LourdesZamora <lourdes.zamora@unitec.edu>
2018-02-19 19:01:24 -0600

    bootstrap install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
  get 'locations/new'
- root to: "home#index"
+ get 'locations/delete'
+  get 'locations/edit'
+   get 'locations/show'
+ root to: "locations#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
