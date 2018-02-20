e28be067e442bff0ea21bc62aa38173825644f2e
LourdesZamora <lourdes.zamora@unitec.edu>
2018-02-19 22:53:03 -0600

    shit

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
  post "locations" => "locations#create"
  get 'locations/delete'
   get 'locations/edit'
+  root 'locations#index'
    get 'locations/show'
   get 'locations/index'
+    resources :locations do
+        resources :recordings
+    end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
